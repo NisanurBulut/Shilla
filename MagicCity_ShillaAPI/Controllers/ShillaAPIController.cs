71a0bde308213dd03043b7e05cde9a1bebe1a2ed
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-21 11:37:24 +0300

    defined get item Shilla

--- MagicCity_ShillaAPI/Controllers/ShillaAPIController.cs
+++ MagicCity_ShillaAPI/Controllers/ShillaAPIController.cs
@@ -1,17 +1,23 @@
-﻿using MagicCity_ShillaAPI.Models;
+﻿using MagicCity_ShillaAPI.Data;
+using MagicCity_ShillaAPI.Models;
 using MagicCity_ShillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicCity_ShillaAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ShillaAPIController : ControllerBase
     {
         [HttpGet]
         public IEnumerable<ShillaDto> GetShillas()
         {
-            
+            return ShillaStore.shillaList;
+        }
+        [HttpGet("id")]
+        public ShillaDto GetShilla(int id)
+        {
+            return ShillaStore.shillaList.FirstOrDefault(u => u.Id == id);
         }
     }
 }
