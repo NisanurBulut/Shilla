678f5bee9393b9e7c51560cd33d8d4b129391f70
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-21 11:11:10 +0300

    first api definition

--- MagicCity_ShillaAPI/Controllers/ShillaAPIController.cs
+++ MagicCity_ShillaAPI/Controllers/ShillaAPIController.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicCity_ShillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicCity_ShillaAPI.Controllers
 {
+    [Route("api/ShillaAPI")]
     [ApiController]
     public class ShillaAPIController : ControllerBase
     {
+        public IEnumerable<Shilla> GetShillas()
+        {
+            return new List<Shilla>
+            {
+                new Shilla
+                {
+                    Id = 1,
+                    Name = "Pool View"
+                },
+                new Shilla
+                {
+                    Id = 2,
+                    Name = "Beach View"
+                }
+            };
+        }
     }
 }
---
+++ MagicCity_ShillaAPI/Models/Shilla.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicCity_ShillaAPI.Models
+{
+    public class Shilla
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
