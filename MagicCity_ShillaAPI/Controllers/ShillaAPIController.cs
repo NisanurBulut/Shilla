6b62c5bccd148dfa9a528c7a38a93888b15b91a5
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-26 20:32:01 +0300

    create Entity http post

--- MagicCity_ShillaAPI/Controllers/ShillaAPIController.cs
+++ MagicCity_ShillaAPI/Controllers/ShillaAPIController.cs
@@ -15,7 +15,7 @@ namespace MagicCity_ShillaAPI.Controllers
             return Ok(ShillaStore.shillaList);
         }
         [HttpGet("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(ShillaDto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ShillaDto))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<ShillaDto> GetShilla(int id)
@@ -30,6 +30,22 @@ namespace MagicCity_ShillaAPI.Controllers
                 return NotFound();
             }
             return Ok(shillaItem);
+        }
+
+        [HttpPost]
+        public ActionResult<ShillaDto> CreateShilla([FromBody] ShillaDto shillaDto)
+        {
+            if (shillaDto == null)
+            {
+                return BadRequest(shillaDto);
+            }
+            if (shillaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            shillaDto.Id = ShillaStore.shillaList.OrderByDescending(a => a.Id).FirstOrDefault().Id + 1;
+            ShillaStore.shillaList.Add(shillaDto);
+            return Ok(shillaDto)
         }
     }
 }
