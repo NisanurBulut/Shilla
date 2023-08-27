9519f1b882b9cf1b6aa28e87ce1e175e0167fa43
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-27 19:05:47 +0300

    implemented Shilla Repository

--- MagicCity_ShillaAPI/Repository/IRepository/IShillaRepository.cs
+++ MagicCity_ShillaAPI/Repository/IRepository/IShillaRepository.cs
@@ -5,8 +5,8 @@ namespace MagicCity_ShillaAPI.Repository.IRepository
 {
     public interface IShillaRepository
     {
-        Task<List<Shilla>> GetAll(Expression<Func<Shilla>> filter = null);
-        Task<Shilla> Get(Expression<Func<Shilla>> filter = null, bool tracked = true);
+        Task<List<Shilla>> GetAll(Expression<Func<Shilla,bool>> filter = null);
+        Task<Shilla> Get(Expression<Func<Shilla,bool>> filter = null, bool tracked = true);
 
         Task Create(Shilla shillaEntity);
         Task Remove(Shilla shillaEntity);
