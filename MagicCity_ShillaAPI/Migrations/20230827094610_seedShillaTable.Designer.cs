55dbda3bed259c4ea17f2527ee3a22cb7d91a8fe
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-27 12:48:28 +0300

    seed db with migration

--- MagicCity_ShillaAPI/Migrations/20230827094610_seedShillaTable.Designer.cs
+++ MagicCity_ShillaAPI/Migrations/20230827094610_seedShillaTable.Designer.cs
@@ -4,6 +4,7 @@ using MagicCity_ShillaAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MagicCity_ShillaAPI.Migrations
 {
     [DbContext(typeof(ShillaDbContext))]
-    partial class ShillaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230827094610_seedShillaTable")]
+    partial class seedShillaTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
