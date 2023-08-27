b03847db7db5750381169ae13d00f400713881d2
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-27 18:49:44 +0300

    inserted IshillaRepository

---
+++ MagicCity_ShillaAPI/Repository/IRepository/IShillaRepository.cs
@@ -0,0 +1,15 @@
+﻿using MagicCity_ShillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicCity_ShillaAPI.Repository.IRepository
+{
+    public interface IShillaRepository
+    {
+        Task<List<Shilla>> GetAll(Expression<Func<Shilla>> filter = null);
+        Task<Shilla> Get(Expression<Func<Shilla>> filter = null, bool tracked = true);
+
+        Task Create(Shilla shillaEntity);
+        Task Remove(Shilla shillaEntity);
+        Task Save();
+    }
+}
