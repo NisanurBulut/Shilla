ecfec9d15b0778499d3b6337540e5c61e0d36ff3
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-08-30 20:48:53 +0300

    defined baseService

--- MagicCity_ShillaWEB/Models/APIRequest.cs
+++ MagicCity_ShillaWEB/Models/APIRequest.cs
@@ -1,6 +1,11 @@
-﻿namespace MagicCity_ShillaWEB.Models
+﻿using static MagicShilla_Utility.SD;
+
+namespace MagicCity_ShillaWEB.Models
 {
     public class APIRequest
     {
+        public ApiType apiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
     }
 }
--- MagicCity_ShillaWEB/Services/IServices/IBaseService.cs
+++ MagicCity_ShillaWEB/Services/IServices/IBaseService.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+﻿using MagicCity_ShillaWEB.Models;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace MagicCity_ShillaWEB.Services.IServices
 {
     public interface IBaseService
     {
-       
+
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
     }
 }
