6e882be7416b05035efb415e8d7b42a1bcba4fbc
Nisanur Bulut <nisanur.bulut@siemens.com>
2023-09-13 14:37:15 +0300

    implemented user repository

---
+++ MagicCity_ShillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using MagicCity_ShillaAPI.Repository.IRepository;
+using MagicShilla_Utility.Data;
+using MagicShilla_Utility.Dto;
+using MagicShilla_Utility.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicCity_ShillaAPI.Repository
+{
+    public class UserRepository : Repository<LocalUser>, IUserRepository
+    {
+        private ShillaDbContext _dbContext;
+        public UserRepository(ShillaDbContext dbContext) : base(dbContext)
+        {
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> IsUniqueUserAsync(string userName)
+        {
+            var userEntity = await _dbContext.LocalUsers.FirstOrDefaultAsync(a => a.UserName == userName);
+            if (userEntity == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Task<LoginResponseDto> LoginUserAsync(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> RegisterAsync(RegisterationRequestDto registerationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
